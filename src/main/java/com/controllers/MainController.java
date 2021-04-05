2e44bcc5d01a9fcd40da2cde8e97d457d6d2cf86
Yaroslav <tangut1998@gmail.com>
2021-04-05 14:04:38 +0400

    added ability to add client.

--- src/main/java/com/controllers/MainController.java
+++ src/main/java/com/controllers/MainController.java
@@ -2,10 +2,15 @@ package com.controllers;
 
 import com.entities.Client;
 import com.repos.ClientRepos;
+import com.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 
 @Controller
@@ -14,10 +19,26 @@ public class MainController {
     @Autowired
     ClientRepos clientRepo;
 
+    @Autowired
+    ClientService clientService;
+
     @GetMapping("/")
+    public String greetings(){
+        return "redirect:/main";
+    }
+
+    @GetMapping("/main")
     public String main(Model model){
         Iterable<Client> clients = clientRepo.findAll();
         model.addAttribute("clients", clients);
         return "main";
+    }
+
+    @PostMapping("/main")
+    public String addClient(@RequestParam String name, @RequestParam String phoneNumber, @RequestParam String email,
+                            @RequestParam String passportNumber, Map<String, Object> model){
+        Client client = new Client(name, phoneNumber, email, passportNumber);
+        clientService.addClient(client);
+        return "redirect:/main";
     }
 }
